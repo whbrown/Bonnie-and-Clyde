86ad8a1354c0d2f53a94713cbf8b4157cf2cbdb7
William Brown <hce@WmHB.local>
2019-10-29 16:33:21 +0100

    Create vehicle types object to handle NPC properties

--- src/game/constants.js
+++ src/game/constants.js
@@ -1,2 +1,35 @@
 const WIDTH = 720;
 const HEIGHT = 720;
+
+const carTypes = {
+  SUV: {
+    imgPath: './src/game/assets/car4.png',
+    health: 150,
+    drivingStyle: 'conservative',
+  },
+  Buick: {
+    imgPath: './src/game/assets/car5.png',
+    health: 120,
+    drivingStyle: 'conservative',
+  },
+  'Kissel White Eagle': {
+    imgPath: './src/game/assets/car6.png',
+    health: 100,
+    drivingStyle: 'standard',
+  },
+  'Ford Model B': {
+    imgPath: './src/game/assets/car7.png',
+    health: 100,
+    drivingStyle: 'standard',
+  },
+  'Ford Model A': {
+    imgPath: './src/game/assets/car8.png',
+    health: 80,
+    drivingStyle: 'slow',
+  },
+  Cadillac: {
+    imgPath: './src/game/assets/car9.png',
+    health: 120,
+    drivingStyle: 'aggressive',
+  },
+};
